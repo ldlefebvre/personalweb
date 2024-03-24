1a840ec76ee4b056ec4c7b0c633e7fe8742ff724
Laurent Lefebvre <laurentlefebvre8@hotmail.com>
2024-03-23 20:27:27 -0400

    add home video and whole set up

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,6 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
+
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'videos')
+Rails.application.config.assets.precompile += %w( *.mov )
