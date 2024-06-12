11d3695bdf108bef27dbc2b3ef568db635fb3a1f
Laurent Lefebvre <laurentlefebvre8@hotmail.com>
2024-06-12 18:34:51 -0400

    Fix assets configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
+  config.assets.precompile += %w( logo.png logosquare.png )
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
