b4a6ed77e9d758551d1fe9a77a126c8017f68e3e
Laurent Lefebvre <laurentlefebvre8@hotmail.com>
2024-06-11 20:18:48 -0400

    fix the ui for the -my projects- tab and create sub rendered views for each project

--- app/javascript/controllers/project_controller.js
+++ app/javascript/controllers/project_controller.js
@@ -5,13 +5,30 @@ export default class extends Controller {
     document.querySelectorAll('.project-details-link').forEach((link) => {
       link.addEventListener('click', (event) => {
         event.preventDefault();
-        var targetId = link.getAttribute('href').substring(1);
-        document.querySelectorAll('.project-details-section').forEach((section) => {
-          section.style.display = 'none';
-        });
-        document.getElementById(targetId).style.display = 'block';
-        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        this.showProjectDetails(link.getAttribute('href').substring(1), true);
       });
     });
+
+    document.querySelectorAll('.nav-button').forEach((button) => {
+      button.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.showProjectDetails(button.getAttribute('href').substring(1), true);
+      });
+    });
+
+    this.showProjectDetails('exad-details', false); // Display EXAD details by default without scrolling
+  }
+
+  showProjectDetails(targetId, scroll) {
+    document.querySelectorAll('.project-details-section').forEach((section) => {
+      section.style.display = 'none';
+    });
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      targetSection.style.display = 'block';
+      if (scroll) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
   }
 }
