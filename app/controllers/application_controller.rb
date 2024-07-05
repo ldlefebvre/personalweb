8bcd79761ae52c77a11c29349b1691971ba2b455
Laurent Lefebvre <laurentlefebvre8@hotmail.com>
2024-07-05 16:27:27 -0400

    Set up domain redirects and force SSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_to_https_and_www
+
+  private
+
+  def redirect_to_https_and_www
+    if request.host == 'laurentlefebvre.me'
+      redirect_to "https://www.laurentlefebvre.me#{request.fullpath}", status: :moved_permanently
+    end
+  end
 end
